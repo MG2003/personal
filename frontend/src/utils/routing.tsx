f875c11568d24d4a2e9b4da0ce5b83b2bbae0cfc
MG2003 <max.guo2014@gmail.com>
2024-08-05 00:26:34 -0400

    rebuild

--- frontend/src/utils/routing.tsx
+++ frontend/src/utils/routing.tsx
@@ -7,10 +7,7 @@ import About from '../pages/about';
 import Boba from '../pages/boba';
 import Art from '../pages/art';
 import Cs from '../pages/cs';
-import Experience from '../pages/experience';
 import { Project, Gallery } from '../layouts/portfolio/project_blurb';
-
-import { PROJECTS } from './consts';
 
 import ArtGrid from '../layouts/portfolio/art_pf';
 
